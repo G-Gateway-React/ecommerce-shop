c6ae779143aa26d6abac13326a4fede9d4f332e8
husamkamal <husamkmal94@gmail.com>
2022-07-29 15:08:10 +0300

    remove unnesesry lins

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from './Screens/Home/Home';
-import GlobalStyle from './Global.style';
 function App() {
   return (
     <div className="App">
--- src/Components/LinkSectionFooter/LinkSectionFooter.tsx
+++ src/Components/LinkSectionFooter/LinkSectionFooter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, {  useState } from "react"
 import LinkColumn from "./LinkColumn";
 import { LinkSectionFooterContainer } from "./LinkSectionFooter.style";
 
